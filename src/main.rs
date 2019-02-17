b13e52a6c75969c5a66004628dd8ce19075ecf42
Dillon <LordBlackHole@gmail.com>
2019-02-16 19:56:25 -0500

    Some micro optimizations to my pow test case.

--- src/main.rs
+++ src/main.rs
@@ -60,33 +60,29 @@ fn build_pow() -> BitFunction {
   let mut body: Vec<Instruction> = Vec::new();
   // let i = 1;
   body.push(Instruction::LoadConstFloat {value: 1.0}); // Init answer
-  body.push(Instruction::StoreValue {local: 2}); // store answer in empty third local
 
   // while (power != 0) {
   body.push(Instruction::LoadConstFloat {value: 0.0}); // load zero for check
   body.push(Instruction::LoadValue {local: 1}); // load from argument 1, the power
-  body.push(Instruction::IfEqual{jump: 10}); // check if power remaining is zero
+  body.push(Instruction::IfEqual{jump: 8}); // check if power remaining is zero
 
 
   //   i = i * base;
-  body.push(Instruction::LoadValue {local: 2}); // load answer to stack
   body.push(Instruction::LoadValue {local: 0}); // load base to stack
   body.push(Instruction::CallBuiltIn {func_id: 1, shape_id: 0}); // call Core.*
-  body.push(Instruction::StoreValue {local: 2}); // store to answer
   //   power = power - 1;
   body.push(Instruction::LoadValue {local: 1}); // load counter to stack
   body.push(Instruction::LoadConstFloat {value: -1.0}); // load negative one to subtract
   body.push(Instruction::CallBuiltIn {func_id: 0, shape_id: 0}); // call Core.+ with -1 to subtract one
   body.push(Instruction::StoreValue {local: 1}); // counter has been decremented, store it back in local
-  body.push(Instruction::Jump {jump: -11}); // jump back up to the loop
+  body.push(Instruction::Jump {jump: -9}); // jump back up to the loop
   // }
 
   // return i;
-  body.push(Instruction::LoadValue {local: 2}); // load answer to stack
   body.push(Instruction::Return); // return answer
 
   return BitFunction {
-    max_locals: 3,
+    max_locals: 2,
     function_shape: Shape::SimpleFunctionShape {
       args: vec![float_shape.clone(), float_shape.clone()],
       result: Box::new(float_shape)
