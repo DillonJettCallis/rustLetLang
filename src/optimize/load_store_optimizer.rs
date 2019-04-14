89d265563bf0c83b28e465af8e24345203870014
Dillon <LordBlackHole@gmail.com>
2019-04-14 16:46:33 -0400

    Added new do_dup flat to load_store opt to help reduce unnessessary loads.

--- src/optimize/load_store_optimizer.rs
+++ src/optimize/load_store_optimizer.rs
@@ -1,5 +1,15 @@
 use ir::{IrFunction, Ir};
 
+/**
+* Finds the pattern of
+* Store(x)
+* Load(x)
+*
+* If this is followed by a Free(x)
+*   Remove all three. The variable is never used again, don't bother storing it.
+* else
+*   Remove the Load(x) and insert a Duplicate before store. Duplicate should be cheaper than Load.
+*/
 pub fn load_store_opt(func: &mut IrFunction) {
   load_store(&mut func.body);
 }
@@ -7,6 +17,7 @@ pub fn load_store_opt(func: &mut IrFunction) {
 fn load_store(body: &mut Vec<Ir>) {
   let mut index = 0usize;
   let mut do_remove = false;
+  let mut do_dup = false;
 
   while index < body.len() - 2 {
     if let Ir::StoreValue {local: ref store} = body[index] {
@@ -14,8 +25,10 @@ fn load_store(body: &mut Vec<Ir>) {
         if store == load {
           if let Ir::FreeLocal {local: ref free} = body[index + 2] {
             if load == free {
-              do_remove = true
+              do_remove = true;
             }
+          } else {
+            do_dup = true;
           }
         }
       }
@@ -27,8 +40,13 @@ fn load_store(body: &mut Vec<Ir>) {
     }
 
     if do_remove {
-      body.drain(index..index + 2);
+      body.drain(index..index + 3);
       do_remove = false;
+    } else if do_dup {
+      body.remove(index + 1);
+      body.insert(index, Ir::Duplicate);
+      index += 1;
+      do_dup = false;
     } else {
       index += 1;
     }
